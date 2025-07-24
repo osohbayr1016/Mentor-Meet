86448ae54b92cdf4fe6acc53a8ba5736182834cf
baabar02 <baabar02githu.com>
2025-07-24 17:52:46 +0800

    fix

--- server/controller/mentor-signup.ts
+++ server/controller/mentor-signup.ts
@@ -3,12 +3,21 @@ import { Request, Response } from "express";
 
 export const MentorSignUp = async (req: Request, res: Response) => {
   const { email, password, phoneNumber } = req.body;
-  if (!email || !password || !phoneNumber || typeof email != "string" || typeof password !=="string" || typeof phoneNumber !== "number") {
-    res
-      .status(400)
-      .send({ message: "Имайл, нууц үг, утасны дугаар шаардлагатай."});
+  if (
+    !email ||
+    !password ||
+    !phoneNumber ||
+    typeof email != "string" ||
+    typeof password !== "string"
+  ) {
+    res.status(400).send({ message: "Имайл, нууц үг шаардлагатай." });
     return;
   }
 
-  const isEmailExisted = 
+  const isEmailExisted = await MentorModel.findOne({ email });
+  if (!isEmailExisted) {
+    const hashedPassword = await bcrypt.hashsync(password, 10);
+    await MentorModel.create({ email, password: hashedPassword });
+    res.status(200).send({ message: "Бүртгэл амжилттай" });
+  }
 };
