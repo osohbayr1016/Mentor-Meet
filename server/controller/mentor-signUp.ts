dafbce37b462169d669f4feed59d88e2025295e7
tulgaa-12 <e7016307@gmail.com>
2025-07-24 20:19:10 +0800

    up

---
+++ server/controller/mentor-signUp.ts
@@ -0,0 +1,14 @@
+import { MentorType } from "../model/mentor-model";
+import { Request, Response } from "express";
+
+export const MentorSignUp = async (req: Request, res: Response) => {
+  const { email, password, phoneNumber } = req.body;
+  if (!email || !password || !phoneNumber || typeof email != "string" || typeof password !=="string" || typeof phoneNumber !== "number") {
+    res
+      .status(400)
+      .send({ message: "Имайл, нууц үг, утасны дугаар шаардлагатай."});
+    return;
+  }
+
+
+};
