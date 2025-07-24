0f82ead59181b01216f617f9dd3a614c14d8c003
tulgaa-12 <e7016307@gmail.com>
2025-07-24 16:09:47 +0800

    mentor bolon student shema

--- server/model/student-model.ts
+++ server/model/student-model.ts
@@ -1,7 +1,4 @@
-import { ObjectId } from "mongoose"
-
-
-
+import { ObjectId, Schema } from "mongoose"
 
 
   export type StudentType = {
@@ -15,6 +12,21 @@ import { ObjectId } from "mongoose"
     role:string,
     createAt: Date,
     updateAt: Date,
-    meetingHistory: ObjectId,
-    bookedHistory:ObjectId
+    meetingHistory: ObjectId[],
+    bookedHistory:ObjectId[]
   }
+
+
+const StudentSchema = new Schema<StudentType>({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, unique: true },
+    phoneNumber: { type: Number, required: false },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    nickname: { type: String, required: false },
+    role: { type: String, required: true }, 
+    createAt: { type: Date, default: Date.now },
+    updateAt: { type: Date, default: Date.now },
+    meetingHistory: { type: Schema.ObjectId, ref: 'Meeting' }, 
+    bookedHistory: { type: Schema.ObjectId, ref: 'Booking' } 
+})
