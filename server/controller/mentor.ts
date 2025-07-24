881a203a456d4974fc637d29dcacfaeee15ea234
baabar02 <baabar02githu.com>
2025-07-24 17:10:21 +0800

    s

--- server/controller/mentor.ts
+++
@@ -1,8 +0,0 @@
-import { MentorType } from "../model/mentor-model";
-import expess from "express";
-
-export const MentorSignUp = async (req: Request, res: Response) => {
-  const { email, password, phoneNumber } = req.body;
-  if (!email || !password || !phoneNumber) {
-  }
-};
