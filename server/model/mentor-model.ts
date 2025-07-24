0fec3bda5bf48756cc8fed321c68b1041503061f
baabar02 <baabar02githu.com>
2025-07-24 14:39:17 +0800

    mentor

---
+++ server/model/mentor-model.ts
@@ -0,0 +1,35 @@
+import { Schema } from "mongoose";
+
+export type MentorType = {
+  _id: string;
+  email: string;
+  password: string;
+  phoneNumber?: number;
+  firstName: string;
+  lastName: string;
+  nickName?: string;
+  role: string; //
+  image: string;
+  bio: string;
+  profession: string;
+  education: EducationType;
+  experience: string;
+  calendar: string; //
+  category: string; //
+  rating: number;
+  review: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type EducationType = {
+  schoolName: string;
+  major: string;
+  endedYear: Date;
+};
+
+export type ExperienceType = {
+  work: string;
+  position: string;
+  careerDuration: Date;
+};
