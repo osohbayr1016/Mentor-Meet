a39e84d0950b39445aed860ba571aa4ce1d47d15
tulgaa-12 <e7016307@gmail.com>
2025-07-28 11:29:55 +0800

    update

--- server/router/student-router.ts
+++ server/router/student-router.ts
@@ -5,6 +5,7 @@ import {
   createPassword,
   StudentNameNumber,
 } from "../controller/student-signup";
+import { StudentLogin } from "../controller/student-login";
 
 export const StudentRouter = Router();
 
@@ -12,3 +13,4 @@ StudentRouter.post("/Checkemail", Checkemail);
 StudentRouter.post("/checkOtp", checkOtp);
 StudentRouter.post("/createPassword", createPassword);
 StudentRouter.post("/StudentNameNumber", StudentNameNumber)
+StudentRouter.post("/Studentlogin",StudentLogin)
