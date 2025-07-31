68cf08e00479c2e86045b495f1d4820c82d87839
baabar02 <baabarmx@gmail.com>
2025-07-31 10:33:25 +0800

    qwert

--- server/router/calendar-router.ts
+++ server/router/calendar-router.ts
@@ -5,6 +5,6 @@ import { getMentorCalendar } from "../controller/mentor-get-calendar";
 
 export const CalendarRouter = Router();
 
-CalendarRouter.post("/calendar", MentorTokenChecker, UpdateMentorAvailability);
+CalendarRouter.post("/Calendar",UpdateMentorAvailability);
 
 CalendarRouter.get("/calendar/:mentorId", getMentorCalendar);
