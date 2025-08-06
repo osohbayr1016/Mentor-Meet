e61f7d10b478363a974bfae38f7afa43f296c50e
baabar02 <baabar02githu.com>
2025-08-06 10:36:27 +0800

    get-booking

--- server/router/booking-router.ts
+++ server/router/booking-router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { createBooking } from "../controller/create-booking";
+import { getBooking } from "../controller/get-bookings";
 
-export const BookingRouter=Router();
+export const BookingRouter = Router();
 
-BookingRouter.post("/booking", createBooking)
+BookingRouter.post("/booking", createBooking);
+BookingRouter.get("/get-booking", getBooking);
